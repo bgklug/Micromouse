aabd0d73005196c9e6502471cc4ead171b89e4ab
Dillon <ninjamills@hotmail.com>
2014-11-15 11:50:31 -0900

    new maze generation code (build)

--- MazeGenerationBuild.c
+++ MazeGenerationBuild.c
@@ -16,11 +16,22 @@ int main(void)
 	int gen = 0;		//generation count
 	int goodGen = 0;	//whether or not the maze is solvable
 	clock_t tStart=clock(), tEnd=clock();	//starting and ending time
-	srand(time(NULL));    
+	srand(clock());    
 	
 	//----------//----------//MAZE GENERATION//----------//----------//
+
 	//----------//Constants//----------//
 	//manually places constants into the maze array
+
+	//Clean Slate//
+	//starts the maze without having any walls anywhere
+	for(int i=0; i<=16; i++){
+		for(int j=0; j<=16; j++){
+			for(int k=0; k<4; k++){
+				b[i][j][k] = 0;
+			}
+		}
+	}
 
 	//Center Walls//
 	//makes the center squares have walls on all sides
@@ -78,7 +89,20 @@ int main(void)
 		for(int j=0; j<16; j++){
 			goodGen = 0;
 			while(!goodGen){	//makes sure maze is solvable
-				
+
+				//----------//Seed//----------//
+				//prevents bad rand() seed where it makes every square the same
+				int goodSeed = 0;
+				if(i>0){
+					for(int k=0; k<4; k++){
+						if(b[i][j][k]==b[i-1][j][k]){
+							goodSeed = 1;}
+					}
+				}
+				if(!goodSeed||(goodSeed==15)){ 
+					srand(clock()); 
+				}
+
 				//----------//Walls//----------//
 				//build a random maze
 				
@@ -88,41 +112,103 @@ int main(void)
 					if(i%2){
 						if(j%2){
 							b[i][j][k] = rand() % 2;
+						}else{
+							b[i][j][k] = 0;
 						}
 					}else{
 						if(j%2){
+							b[i][j][k] = 0;
+						}else{
 							b[i][j][k] = rand() % 2;
 						}
 					}
 				}
-/*
+
+	//----------//Constants//----------//
+	//This was placed here carelessly to make sure the constants stay as they should be without having to be creative
+	
+	//Center Walls//
+	//makes the center squares have walls on all sides
+	for(int i=7; i<=8; i++){
+		for(int j=7; j<=8; j++){
+			for(int k=0; k<4; k++){
+				b[i][j][k] = 1;
+			}
+		}
+	}
+
+	//Center Opening//
+	//places center walls
+	switch(randWall){
+		case 0: b[7][8][0] = 0; break;
+		case 1: b[8][8][0] = 0; break;
+		case 2: b[8][7][1] = 0; break;
+		case 3: b[8][8][1] = 0; break;
+		case 4: b[7][7][2] = 0; break;
+		case 5: b[8][7][2] = 0; break;
+		case 6: b[7][7][3] = 0; break;
+		case 7: b[7][8][3] = 0; break;
+	}
+
+	//Corners//
+	//places the walls in the four corners
+	b[0][0][0] = 0;
+	b[0][0][1] = 1;
+	b[0][1][2] = 0;
+	b[1][0][3] = 1;
+	b[0][15][1] = 0;
+	b[0][15][2] = 1;
+	b[0][14][0] = 1;
+	b[1][15][3] = 0;
+	b[15][0][0] = 1;
+	b[15][0][3] = 0;
+	b[15][1][2] = 1;
+	b[14][0][1] = 0;
+	b[15][15][2] = 0;
+	b[15][15][3] = 1;
+	b[15][14][0] = 0;
+	b[14][15][1] = 1;
+
+	//Boarder//
+	//make the edges have walls
+	for(int i=0; i<16; i++){
+		b[0][i][3] = 1;
+		b[15][i][1] = 1;
+		b[i][0][2] = 1;
+		b[i][15][0] = 1;
+	}
+
 				//Pegs//
 				//places a wall on every peg
-				if((i<15)&&(i<15)){
-					if(!(b[i][j][1]+b[i][j+1][2]+b[i+1][j][0]+b[i+1][j+1][3])){
+				if((i>0)&&(j>0)){
+					if(!(b[i][j][1]+b[i][j-1][2]+b[i-1][j][0]+b[i-1][j-1][3])){
 						int randPeg = rand()%4;
 						switch(randPeg){
 							case 0: b[i][j][1] = 1; break;
-							case 1: b[i][j+1][2] = 1; break;
-							case 2: b[i+1][j][0] = 1; break;
-							case 3: b[i+1][j+1][3] = 1; break;
+							case 1: b[i][j-1][2] = 1; break;
+							case 2: b[i-1][j][0] = 1; break;
+							case 3: b[i-1][j-1][3] = 1; break;
 						}
 					}
 				}
-*/
+
 				//Check//
 				//changes the values of adjacent walls
-				if(i>0){
-					b[i-1][j][1] = b[i][j][3];
-				}
-				if(i<15){
-					b[i+1][j][3] = b[i][j][1];
-				}
-				if(j>0){
-					b[i][j-1][0] = b[i][j][2];
-				}
-				if(i<15){
-					b[i][j+1][2] = b[i][j][0];
+				for(int k=0; k<16; k++){
+					for(int l=0; l<16; l++){
+						if(k>0){
+							b[k-1][l][1] = b[k][l][3];
+						}
+						if(k<15){
+							b[k+1][l][3] = b[k][l][1];
+						}
+						if(l>0){
+							b[k][l-1][0] = b[k][l][2];
+						}
+						if(l<15){
+							b[k][l+1][2] = b[k][l][0];
+						}
+					}
 				}
 				
 
@@ -131,9 +217,9 @@ int main(void)
 
 				//Reset//
 				//fills all flood array spaces with -1
-				for(int i=0;i<16;i++){
-					for(int j=0;j<16;j++){
-						c[i][j] = -1;
+				for(int k=0;k<16;k++){
+					for(int l=0;l<16;l++){
+						c[k][l] = -1;
 					}
 				}
 
@@ -182,39 +268,39 @@ int main(void)
 				}
 			
 
-			//----------//Debugging//----------//
-			//code to help debug
+				//----------//Debugging//----------//
+				//code to help debug
+	
+				//Print Info//
+				//prints time elapsed, generation count, and goodSeed value to console
+				system("cls");
+				tEnd = clock();
+				printf("Time:\t\t%.2f\n", (double)(tEnd - tStart)/1000);
+				printf("Generation:\t%d\n", gen++);
+				
+				//Print Maze//
+				//prints visual representation of maze
 
-			//Print Info//
-			//prints time elapsed, generation count, and goodSeed value to console
-			system("cls");
-			tEnd = clock();
-			printf("Time:\t\t%.2f\n", (double)(tEnd - tStart)/1000);
-			printf("Generation:\t%d\n", gen++);
-			//printf("goodSeed:\t%d\n", goodSeed);
-			
-			//Print Maze//
-			//prints visual representation of maze
-			printf(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
-			for(int l=0; l<16; l++){
-					printf("\n|");
-				for(int m=0; m<16; m++){
-					
-					//visual representation
-					if(b[l][m][1]){
-						printf("_");}
-					else{
-						printf(" ");}
-					if(b[l][m][0]){
-						printf("|");}
-					else{
-						printf(" ");}
-		
-					//printf("%d ", b[l][m][0]+b[l][m][1]+b[l][m][2]+b[l][m][3]);	//wall count
-		
-					//printf("%d\t",c[l][m]);	//flood value
-				}
+		printf(" _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _");
+		for(int i=0; i<16; i++){
+				printf("\n|");
+			for(int j=0; j<16; j++){
+				
+				//visual representation
+				if(b[i][j][1]){
+					printf("_");}
+				else{
+					printf(" ");}
+				if(b[i][j][0]){
+					printf("|");}
+				else{
+					printf(" ");}
+	
+				//printf("%d ", b[i][j][0]+b[i][j][1]+b[i][j][2]+b[i][j][3]);	//wall count
+	
+				//printf("%d\t",c[i][j]);	//flood value
 			}
+		}
 			}
 		}
 	}
