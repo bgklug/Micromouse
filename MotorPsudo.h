6e66daf077db1d1750518fd289823f0661ba7bd2
Bryant <bgklug@alaska.edu>
2014-11-22 13:12:19 -0900

    sensor code version 1

---
+++ MotorPsudo.h
@@ -0,0 +1,14 @@
+#ifndef MOTORPSUDO_H_INCLUDED
+#define MOTORPSUDO_H_INCLUDED
+
+void moveN(int & dir);
+void moveE(int & dir);
+void moveS(int & dir);
+void moveW(int & dir);
+
+void moveF();
+void moveB();
+void turnR(int & dir);
+void turnL(int & dir);
+
+#endif
---
+++ SensorPsudo.h
@@ -0,0 +1,10 @@
+#ifndef SENSORPSUDO_H_INCLUDED
+#define SENSORPSUDO_H_INCLUDED
+
+int sensorReadL(int dir, const int & m[16][16][5], int row, int col);
+int sensorReadF(int dir, const int & m[16][16][5], int row, int col);
+int sensorReadR(int dir, const int & m[16][16][5], int row, int col);
+void senseWall(int dir, int row, int col, const int & gen[16][16][5], int & mou[16][16][5]);
+
+
+#endif
