8cc1e8705983c2304aec41e2fd237e2210acb745
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2014-11-22 17:05:19 -0900

    Fixed Left-Side Bug
    -1%4 doesn't appear to work, so I changed all instances of "(dir - 1)%4" to "(dir + 3)%4"

--- SensorPseudo.cpp
+++ SensorPseudo.cpp
@@ -1,6 +1,6 @@
 #include "SensorPseudo.h"
 int sensorReadL(int dir, const int m[16][16][5], int row, int col){
-	return m[row][col][(dir - 1)% 4];//will return 1 for wall or 0 for space
+	return m[row][col][(dir + 3)% 4];//will return 1 for wall or 0 for space
 }
 int sensorReadF(int dir, const int m[16][16][5], int row, int col){
 	return m[row][col][dir];
@@ -9,7 +9,7 @@ int sensorReadR(int dir, const int m[16][16][5], int row, int col){
 	return m[row][col][(dir + 1)% 4];
 }
 void senseWall(int dir, int row, int col, const int gen[16][16][5], int mou[16][16][5]){
-	mou[row][col][(dir-1)%4] = sensorReadL(dir, gen, row, col);
+	mou[row][col][(dir+3)%4] = sensorReadL(dir, gen, row, col);
 	mou[row][col][dir] = sensorReadF(dir, gen, row, col);
 	mou[row][col][(dir+1)%4] = sensorReadR(dir, gen, row, col);
 }
