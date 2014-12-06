cd76e867e066c87131b0cf300a7d7948faa8fb94
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2014-12-06 12:46:30 -0900

    Create WallSituations.h

---
+++ WallSituations.h
@@ -0,0 +1,9 @@
+#ifndef WALLSITUATIONS_H
+#define WALLSITUATIONS_H
+
+char wallSet(bool, bool, bool, bool);
+bool wallExists(char, char);
+char wallChange(char, char, bool);
+char wallRotate(char, bool);
+
+#endif
