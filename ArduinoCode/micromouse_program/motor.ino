78e3201f9ff8d654d91ff5bedbc30cb1e53e96f8
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2015-04-25 14:22:35 -0800

    Update motor.ino

--- ArduinoCode/micromouse_program/motor.ino
+++ ArduinoCode/micromouse_program/motor.ino
@@ -63,6 +63,7 @@ void moveW(byte & dir, byte & mouRow, byte & mouCol){
 }
 
 void moveF(byte dir, byte & mouRow, byte & mouCol){
+  forward(spd/2);
   if(dir == 0){
     ++mouRow;
   }else if(dir == 2){
@@ -79,9 +80,80 @@ void moveB(){
 }
 
 void turnR(byte & dir){
+  turn_right(spd/2);
   dir = (dir + 1)%4;
 }
 
 void turnL(byte & dir){
+  turn_left(spd/2);
   dir = (dir + 3)%4;
+}
+
+
+
+
+
+void forward(float spd)
+{
+  int i=0;
+  motorR.setSpeed(spd*-1);
+  motorL.setSpeed(spd*-1);
+  if(counterForward >= 4)
+  {
+    counterForward -= 4;
+    motorR.runSpeed();
+    motorL.runSpeed();
+  }
+  else if(counterForward <= -4)
+  {
+    i++;
+   counterForward +=4; 
+  }
+  while (i<143)
+  {
+    
+    motorR.runSpeed();
+    motorL.runSpeed();
+    i++;    
+    delay(7);
+  }
+  counterForward++;
+}
+
+void backward(float spd)
+{
+  forward(spd*-1);
+  counterForward -= 2;
+}
+
+void turn_right(float spd)
+{
+  int i=0;
+  motorR.setSpeed(spd*-1);
+  motorL.setSpeed(spd);
+  if(counterTurn > 3)
+  {
+    counterTurn -=4;
+    i--;
+  }
+  else if(counterTurn < -3)
+  {
+    counterTurn +=4;
+    i++;
+  }
+  while(i<66) //alter this value based on the amount of rotation
+  {
+    motorR.runSpeed();
+    motorL.runSpeed();
+    i++;
+    delay(7);
+  }
+  counterTurn++;
+}
+
+void turn_left(float spd)
+{
+  turn_right(spd*-1);
+  counterTurn--;
+  counterTurn--;
 }
