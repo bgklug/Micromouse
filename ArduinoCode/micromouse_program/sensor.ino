0c0250ebde479eb4d125e704c370b9f252be553c
BillNyeTheScienceGuy <BillNyeTheScienceGuy@users.noreply.github.com>
2015-04-25 12:44:10 -0800

    Update and rename sensor to sensor.ino

---
+++ ArduinoCode/micromouse_program/sensor.ino
@@ -0,0 +1,40 @@
+byte sensorPin[5] = {0, 1, 2, 3, 4};  // left, leftdiag, center, rightdiag, right
+int sensorValue[5];
+long offset[5];
+
+int threshold = -10;
+
+void sensorSetup() {
+  int iterations = 1000;
+  byte time = 2;  // seconds
+  for (byte i = 0; i < 5; i++) {  // reset offset values
+    offset[i] = 0;
+  }
+  
+  for (int i = 0; i < iterations; i++) {  // make 'iteration' number of readings
+    for (byte i = 0; i < 5; i++) {  // add up all the readings over the defined time
+      offset[i] += analogRead(sensorPin[i]);
+    }
+    delay(time*1000/iterations);
+  }
+  for (byte i = 0; i < 5; i++) {  // divide by the number of iterations to get the average
+    offset[i] /= iterations;
+  }
+  
+  for (byte i = 0; i < 5; i++) {  // print all offsets
+    Serial.println(offset[i]);
+  }
+}
+
+boolean sensorRead(byte side) {
+//  Serial.print("(");
+//  Serial.print(analogRead(sensorPin[side]));
+//  Serial.print(")");
+  return (analogRead(sensorPin[side]) - offset[side]) > threshold;  // will return 1 for wall or 0 for space
+}
+
+void senseWall(byte dir, byte row, byte col, char m[SIZE][SIZE][5]) {
+  m[row][col][(dir + 3)%4] = sensorRead(4);  // sensorRead LEFT
+  m[row][col][dir] = sensorRead(2);          // sensorRead FORWARD
+  m[row][col][(dir + 1)%4] = sensorRead(0);  // sensorRead RIGHT
+}
