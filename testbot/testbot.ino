3f00958c146a11fb2fda0d335033fd3962872610
bgklug <bgklug@alaska.edu>
2016-04-02 14:03:02 -0800

    Added P term for path correction

--- testbot/testbot.ino
+++ testbot/testbot.ino
@@ -7,6 +7,10 @@ int m1A = 5;
 int m1B = 6;
 int m2A = 9;
 int m2B = 10;
+float baselineGyroZ;
+bool first = true;
+
+#define DEBUG true
 
 Adafruit_L3GD20_Unified gyro = Adafruit_L3GD20_Unified(20);
 
@@ -18,40 +22,67 @@ void setup()
   pinMode(m2B, OUTPUT);
   pinMode(speedPin, OUTPUT);
 
+  if(DEBUG){
+    Serial.begin(9600);
+  }
   
   gyro.enableAutoRange(true);
   /* Initialise the sensor */
   if(!gyro.begin())
   {
     /* There was a problem detecting the L3GD20 ... check your connections */
-    Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");
+   if(DEBUG){ Serial.println("Ooops, no L3GD20 detected ... Check your wiring!");}
     while(1);
   }
   //event.gyro.z
-  int sum = 0;
+  float sum = 0;
   for(int ii = 0; ii < 10; ++ii){
     sensors_event_t event; 
     gyro.getEvent(&event);
     sum += event.gyro.z;
     delay(5);
   }
+  baselineGyroZ = sum/10;
+  if(DEBUG){
+    Serial.print("Baseline Z = ");Serial.println(baselineGyroZ);
+  }
   
   
 }
 
-
+float w;
+float wold;
+float dw;
+int start;
+int stop;
  
 void loop()
 {
-  int speed = 255 ;
-  setMotor1(speed, 0);
-  setMotor2(speed, 0);
-
+  if(first){start = millis();}
   /* Get a new sensor event */ 
   sensors_event_t event; 
   gyro.getEvent(&event);
+
+  if(!first){
+    wold =w;
+  }
+  else{wold = 0;}
+  w = event.gyro.z-baselineGyroZ;
+
+  if(!first){start = stop;}
+  stop = millis();
+  dw = (w - wold)/(stop-start);
+
+  if(DEBUG){Serial.print("W: "); Serial.println(w);}
+  int speed = 255/2 ;
+  setMotor1(speed-50.0*w, 0);
+  setMotor2(speed+50.0*w, 0);
+  
  
+  if(first)
+  {first= false;}
 }
+
  
 void setMotor1(int speed, boolean reverse)
 {
